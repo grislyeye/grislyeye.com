77ac76f514ff8de9aed7f83a75779c54adaacb30
R.G. Wood <ric@grislyeye.com>
2018-05-31 11:59:01 +0100

    Fixed markdown not parsing on index page

--- helpers/content.js
+++ helpers/content.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
 const path = require('path')
 const fm = require('front-matter')
+const marked = require('marked')
 
 module.exports = function (targetPath) {
   const sourcePath = 'posts/' + path.basename(targetPath, '.html') + '.md'
   const markdown = fs.readFileSync(sourcePath, 'utf8')
-  return fm(markdown).body
+  return marked(fm(markdown).body)
 }
