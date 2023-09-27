6fe7c5a14eb25095fc4a3f51ead840beaf1edbe4
R.G. Wood <ric@grislyeye.com>
2023-09-27 20:41:41 +0100

    [wip] Always hide drafts

--- eleventy.config.drafts.js
+++ eleventy.config.drafts.js
@@ -1,9 +1,6 @@
 function eleventyComputedPermalink() {
-	// When using `addGlobalData` and you *want* to return a function, you must nest functions like this.
-	// `addGlobalData` acts like a global data file and runs the top level function it receives.
 	return (data) => {
-		// Always skip during non-watch/serve builds
-		if(data.draft && !process.env.BUILD_DRAFTS) {
+		if(data.tags && data.tags.includes("drafts")) {
 			return false;
 		}
 
@@ -12,11 +9,8 @@ function eleventyComputedPermalink() {
 };
 
 function eleventyComputedExcludeFromCollections() {
-	// When using `addGlobalData` and you *want* to return a function, you must nest functions like this.
-	// `addGlobalData` acts like a global data file and runs the top level function it receives.
 	return (data) => {
-		// Always exclude from non-watch/serve builds
-		if(data.draft && !process.env.BUILD_DRAFTS) {
+		if(data.tags && data.tags.includes("drafts")) {
 			return true;
 		}
 
@@ -30,21 +24,4 @@ module.exports.eleventyComputedExcludeFromCollections = eleventyComputedExcludeF
 module.exports = eleventyConfig => {
 	eleventyConfig.addGlobalData("eleventyComputed.permalink", eleventyComputedPermalink);
 	eleventyConfig.addGlobalData("eleventyComputed.eleventyExcludeFromCollections", eleventyComputedExcludeFromCollections);
-
-	let logged = false;
-	eleventyConfig.on("eleventy.before", ({runMode}) => {
-		let text = "Excluding";
-		// Only show drafts in serve/watch modes
-		if(runMode === "serve" || runMode === "watch") {
-			process.env.BUILD_DRAFTS = true;
-			text = "Including";
-		}
-
-		// Only log once.
-		if(!logged) {
-			console.log( `[11ty/eleventy-base-blog] ${text} drafts.` );
-		}
-
-		logged = true;
-	});
 }
