1721fe5ee48c2a7b5cf8066936e86e61ad7e221f
R.G. Wood <37806009+rg-wood@users.noreply.github.com>
2023-11-04 12:47:31 +0000

    Display webmentions for micro posts
    References:
    
     * https://rowanmanning.com/posts/webmentions-for-your-static-site/
     * https://brid.gy
     * https://mxb.dev/blog/using-webmentions-on-static-sites/
     * https://webmention.io/dashboard
    
    https://github.com/grislyeye/grislyeye.com/pull/6

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'The Grisly Eye',
-  url: 'https://grislyeye.com/',
+  url: 'https://grislyeye.com',
   language: 'en',
   description: 'I\'m an indie game designer (and forever DM) and this is my blog.',
   author: {
