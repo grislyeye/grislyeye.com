f4eedbb2ba19a6431f1d82916514de7709a37edb
R.G. Wood <ric@grislyeye.com>
2024-03-10 20:09:51 +0000

    Fix: authorship unclear

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'The Grisly Eye',
   url: 'https://grislyeye.com',
   language: 'en',
-  description: 'I\'m an indie game designer (and forever DM) and this is my blog.',
+  description: 'Ric Wood is an indie game designer and forever DM.',
   author: {
     name: 'Ric Wood',
     email: 'rig@grislyeye.com',
