aa6d9409aa44a82fad08b0a27e0aef0550ad01d1
R.G. Wood <ric@grislyeye.com>
2025-01-29 16:40:04 +0000

    [wip] Attempt to group asides

--- _lib/collect.js
+++ _lib/collect.js
@@ -20,17 +20,42 @@ const flattenPlayerMessage = (messages) => {
 };
 
 const consecutiveSameActors = (pre, cur) => {
-  const result = pre.actor === cur.actor || cur.actor === '';
-  console.log('compare - start');
-  console.log(pre);
-  console.log(cur);
-  console.log(result);
-  console.log('compare - end');
-  return result;
+  return pre.actor === cur.actor || cur.actor === '';
+};
+
+const flattenAsides = (messages) => {
+  const baseMessage = messages[0];
+  if (messages.length === 1) return baseMessage;
+
+  if (baseMessage.chat === 'ooc') {
+    const events = messages
+      .filter((m) => m.chat === 'ooc');
+
+    return {
+      actor: baseMessage.actor,
+      type: 'grouped',
+      chat: 'ooc',
+      messages: events
+    };
+  }
+
+  return baseMessage;
+};
+
+const consecutiveOocMessages = (pre, cur) => {
+  return pre.type === cur.actor && cur.actor === 'ooc';
+};
+
+const collectOocMessages = (messages) => {
+  return groupAdjacent(messages, consecutiveOocMessages).map(flattenAsides);
+};
+
+const collectIcMessages = (messages) => {
+  return groupAdjacent(messages, consecutiveSameActors).map(flattenPlayerMessage);
 };
 
 const collectMessages = (messages) => {
-  return groupAdjacent(messages, consecutiveSameActors).map(flattenPlayerMessage);
+  return collectOocMessages(collectIcMessages(messages));
 };
 
 export default collectMessages;
